81c07b6574f4fb4c404bde4f6871e44b83cd423f
Vitalij Dadaschjanz <vitalij.dadaschjanz@zattoo.com>
2017-08-24 10:35:33 +0200

    Initial Commit

---
+++ TVMLCustomListItem/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TVMLCustomListItem
+//
+//  Created by Vitalij Dadaschjanz on 24.08.17.
+//  Copyright © 2017 zattoo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
